a0ce58c1e6adf501e00d591be8927c8d74e48a37
Ilya Siganov <ilya.blan4@gmail.com>
2016-03-12 13:45:44 +0600

    improve server code

--- app/src/main/java/org/seniorsigan/musicroom/APIServer.kt
+++ app/src/main/java/org/seniorsigan/musicroom/APIServer.kt
@@ -16,98 +16,74 @@ class APIServer(val context: Context) : RouterNanoHTTPD(APIServer.PORT) {
         addRoute("/api/search.json", SearchHandler::class.java)
         addRoute("/api/soundcloud.json", SoundCloudHandler::class.java)
         addRoute("/api/vk.json", VkHandler::class.java)
-        addRoute("/api/play.json", BaseHandler::class.java)
+        addRoute("/api/play.json", PlayHandler::class.java)
         addRoute("/(.)+", StaticHandler::class.java, context.assets)
         addRoute("/", StaticHandler::class.java, context.assets)
     }
 
 
 
-    class VkHandler: DefaultHandler() {
-        override fun getMimeType(): String = "application/json"
-
-        override fun getText(): String? {
-            throw UnsupportedOperationException()
-        }
-
-        override fun getStatus() = Response.Status.OK
-
-        override fun get(uriResource: UriResource?, urlParams: MutableMap<String, String>?, session: IHTTPSession?): Response? {
-            Log.d(TAG, "Call VK search")
-            val query = session?.parms?.get("q") ?: ""
-            val tracks = App.vkAPI.search(query)
-            return NanoHTTPD.newFixedLengthResponse(status, mimeType, App.toJson(CommonResponse(true, null, tracks)))
-        }
-    }
-
-    class SoundCloudHandler: DefaultHandler() {
-        override fun getMimeType(): String = "application/json"
-
-        override fun getText(): String? {
-            throw UnsupportedOperationException()
-        }
-
-        override fun getStatus() = Response.Status.OK
-
-        override fun get(uriResource: UriResource?, urlParams: MutableMap<String, String>?, session: IHTTPSession?): Response? {
-            Log.d(TAG, "Call SoundCloud search")
-            val query = session?.parms?.get("q") ?: ""
-            val tracks = App.soundCloud.search(query)
-            return NanoHTTPD.newFixedLengthResponse(status, mimeType, App.toJson(CommonResponse(true, null, tracks)))
-        }
-    }
-
-    class SearchHandler: DefaultHandler() {
-        override fun getMimeType(): String = "application/json"
-
-        override fun getText(): String? {
-            throw UnsupportedOperationException()
-        }
-
-        override fun getStatus() = Response.Status.OK
-
-        override fun get(uriResource: UriResource?, urlParams: MutableMap<String, String>?, session: IHTTPSession?): Response? {
+    class VkHandler: JSONHandler() {
+        override fun handleGet(query: Map<String, String?>): CommonResponse {
             Log.d(TAG, "Call overall search")
-            val query = session?.parms?.get("q") ?: ""
-            val tracks = App.soundCloud.search(query) + App.vkAPI.search(query)
-            return NanoHTTPD.newFixedLengthResponse(status, mimeType, App.toJson(CommonResponse(true, null, tracks)))
+            return try {
+                val q = query["q"] ?: ""
+                val tracks = App.vkAPI.search(q)
+                CommonResponse(true, null, tracks)
+            } catch (e: Exception) {
+                CommonResponse(false, e.message, null)
+            }
         }
     }
 
-    class BaseHandler: DefaultHandler() {
-        override fun getStatus() = Response.Status.OK
-
-        override fun getText(): String? {
-            throw UnsupportedOperationException()
-        }
-
-        override fun getMimeType() = MIME_PLAINTEXT
-
-        override fun get(uriResource: UriResource?, urlParams: MutableMap<String, String>?, session: IHTTPSession?): Response? {
-            if (session?.method == Method.POST) {
-                val body: MutableMap<String, String> = mutableMapOf()
-                session?.parseBody(body)
-                val data = App.parseJson(body["postData"], TrackInfo::class.java) ?: return NanoHTTPD.newFixedLengthResponse(Response.Status.BAD_REQUEST, MIME_PLAINTEXT, "Bad form data")
-                try {
-                    App.historyRepository.create(
-                            artist = data.artist,
-                            title = data.title,
-                            url = data.url,
-                            source = "vk"
-                    )
-                    App.queue.add(TrackForm(
-                            url = data.url,
-                            artist = data.artist,
-                            title = data.title
-                    ))
-                    return NanoHTTPD.newFixedLengthResponse(data.name)
-                } catch (e: Exception) {
-                    Log.e(TAG, e.message, e)
-                    return NanoHTTPD.newFixedLengthResponse(e.message)
-                }
+    class SoundCloudHandler: JSONHandler() {
+        override fun handleGet(query: Map<String, String?>): CommonResponse {
+            Log.d(TAG, "Call SoundCloud search")
+            return try {
+                val q = query["q"] ?: ""
+                val tracks = App.soundCloud.search(q)
+                CommonResponse(true, null, tracks)
+            } catch (e: Exception) {
+                CommonResponse(false, e.message, null)
             }
+        }
+    }
 
-            return NanoHTTPD.newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "Not found")
+    class SearchHandler: JSONHandler() {
+        override fun handleGet(query: Map<String, String?>): CommonResponse {
+            Log.d(TAG, "Call overall search")
+            return try {
+                val q = query["q"] ?: ""
+                val tracks = App.soundCloud.search(q) + App.vkAPI.search(q)
+                CommonResponse(true, null, tracks)
+            } catch (e: Exception) {
+                CommonResponse(false, e.message, null)
+            }
+        }
+    }
+
+    class PlayHandler: JSONHandler() {
+        override fun handlePost(query: Map<String, String?>, body: String?): Any {
+            try {
+                val data = App.parseJson(body, TrackInfo::class.java)
+                        ?: return CommonResponse(false, "Form is empty", null)
+
+                App.historyRepository.create(
+                        artist = data.artist,
+                        title = data.title,
+                        url = data.url,
+                        source = data.source
+                )
+                App.queue.add(TrackForm(
+                        url = data.url,
+                        artist = data.artist,
+                        title = data.title
+                ))
+                return CommonResponse(true, null, data)
+            } catch (e: Exception) {
+                Log.e(TAG, e.message, e)
+                return CommonResponse(false, e.message, null)
+            }
         }
     }
 
@@ -135,6 +111,47 @@ class APIServer(val context: Context) : RouterNanoHTTPD(APIServer.PORT) {
                 return NanoHTTPD.newChunkedResponse(status, getMimeTypeForFile(uri), stream)
             }
             return NanoHTTPD.newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT, "not found")
+        }
+    }
+
+    open class JSONHandler: RouterNanoHTTPD.DefaultHandler() {
+        override fun getMimeType(): String = "application/json"
+
+        override fun getText(): String? {
+            throw UnsupportedOperationException()
+        }
+
+        override fun getStatus() = NanoHTTPD.Response.Status.OK
+
+        override fun get(uriResource: RouterNanoHTTPD.UriResource?, urlParams: MutableMap<String, String>?, session: NanoHTTPD.IHTTPSession?): NanoHTTPD.Response? {
+            return when (session?.method) {
+                Method.GET -> {
+                    Log.d(TAG, "GET: ${session?.uri}")
+                    val query = session?.parms ?: mapOf<String, String?>()
+                    val data = handleGet(query)
+                    NanoHTTPD.newFixedLengthResponse(status, mimeType, App.toJson(data))
+                }
+                Method.POST -> {
+                    Log.d(TAG, "POST: ${session?.uri}")
+                    val query = session?.parms ?: mapOf<String, String?>()
+                    val body: MutableMap<String, String> = mutableMapOf()
+                    session?.parseBody(body)
+                    val data = handlePost(query, body["postData"])
+                    NanoHTTPD.newFixedLengthResponse(status, mimeType, App.toJson(data))
+                }
+                else ->
+                    NanoHTTPD.newFixedLengthResponse(NanoHTTPD.Response.Status.NOT_FOUND, mimeType, "")
+            }
+        }
+
+        open fun handlePost(query: Map<String, String?>, body: String?): Any {
+            Log.w(TAG, "POST method not supported")
+            return Any()
+        }
+
+        open fun handleGet(query: Map<String, String?>): Any {
+            Log.w(TAG, "GET method not supported")
+            return Any()
         }
     }
 
